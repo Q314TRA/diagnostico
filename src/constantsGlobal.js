af5c2b3cc3fc749f0b08aeb818cf0cc665ab0cbc
Luis Miguel Cupitra <miguel@MacBook-Air-de-Luis.local>
2019-04-22 15:16:03 -0500

    env var

--- src/constantsGlobal.js
+++ src/constantsGlobal.js
@@ -1,20 +1,20 @@
 export const API_SERVER = process.env.HOST_API ? process.env.HOST_API : "http://localhost:3000";
 
 // API
-export const GET_ALL_QUESTIOS_API = (process.env.HOST_API ? process.env.HOST_API : "http://localhost:3000") + "/diagnosis/";
-export const GET_VALIDATE_COMPANY_API = (process.env.HOST_API ? process.env.HOST_API : "http://localhost:3000") + "/diagnosis/validateCompany";
-export const PUT_ANSWER_API = (process.env.HOST_API ? process.env.HOST_API : "http://localhost:3000") + "/diagnosis/";
-export const DELETE_ANSWER_API = (process.env.HOST_API ? process.env.HOST_API : "http://localhost:3000") + "/diagnosis/removeAnswer";
+export const GET_ALL_QUESTIOS_API = "https://biotica-api.herokuapp.com/diagnosis/" ;
+export const GET_VALIDATE_COMPANY_API = "https://biotica-api.herokuapp.com/diagnosis/validateCompany" ;
+export const PUT_ANSWER_API = "https://biotica-api.herokuapp.com/diagnosis/" ;
+export const DELETE_ANSWER_API = "https://biotica-api.herokuapp.com/diagnosis/removeAnswer" ;
 
 //SAGA -> REDUCERS
-export const GET_ALL_QUESTIOS_SAGA = "GET_ALL_QUESTIOS_SAGA";
-export const GET_VALIDATE_COMPANY_SAGA = "GET_VALIDATE_COMPANY_SAGA";
-export const PUT_ANSWER_SAGA = "PUT_ANSWER_SAGA";
-export const DELETE_ANSWER_SAGA = "DETELE_ANSWER_SAGA";
+export const GET_ALL_QUESTIOS_SAGA = "GET_ALL_QUESTIOS_SAGA" ;
+export const GET_VALIDATE_COMPANY_SAGA = "GET_VALIDATE_COMPANY_SAGA" ;
+export const PUT_ANSWER_SAGA = "PUT_ANSWER_SAGA" ;
+export const DELETE_ANSWER_SAGA = "DETELE_ANSWER_SAGA" ;
 
 // ACTIONS
-export const SET_ALL_QUESTIOS = "SET_ALL_QUESTIOS";
-export const SET_CURRENT_AXIS = "SET_CURRENT_AXIS";
-export const GET_VALIDATE_COMPANY = "GET_VALIDATE_COMPANY";
-export const PUT_ANSWER = "PUT_ANSWER";
-export const DELETE_ANSWER = "DELETE_ANSWER";
+export const SET_ALL_QUESTIOS = "SET_ALL_QUESTIOS" ;
+export const SET_CURRENT_AXIS = "SET_CURRENT_AXIS" ;
+export const GET_VALIDATE_COMPANY = "GET_VALIDATE_COMPANY" ;
+export const PUT_ANSWER = "PUT_ANSWER" ;
+export const DELETE_ANSWER = "DELETE_ANSWER" ;

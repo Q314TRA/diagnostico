d1b3f01a5f159460d2f997c3c5aa53073c050d7e
Luis Miguel Cupitra <miguel@MacBook-Air-de-Luis.local>
2019-04-22 14:48:52 -0500

    scroll nav

--- src/axis/questionContainerAxis.js
+++ src/axis/questionContainerAxis.js
@@ -1,6 +1,4 @@
-
-
-import React, { Component, createRef } from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
@@ -13,36 +11,39 @@ import TopSection from "./topSection";
 import BottomSection from "./bottomSection";
 import question from './question';
 
-// import { Scroller, scrollInitalState } from "react-skroll";
+import { Scroller, scrollInitalState } from "react-skroll-test";
 
-import ReactSnapScroll from 'react-snap-scroll';
-
-let unmount = false;
 class QuestionContainerAxis extends Component {
     constructor(props) {
         super(props);
         this.state = {
             numItemsPerSection: 3,
-            unmount: false,
+            scroll: scrollInitalState
         }
-        this.sections = {};
-
         this.goToResume = this.goToResume.bind(this);
+
         this.myRef = React.createRef();
-        this.customProgresiveScroll = this.customProgresiveScroll.bind(this);
-        this.pushRef = this.pushRef.bind(this);
-        this.gotToSection = this.gotToSection.bind(this);
 
-        this.pollContent = createRef();
-    }
-
-    componentDidMount() {
     }
 
     goToResume() {
         const { history } = this.props;
-        history.push(`/resume`);
+        // this.setState({
+        //     unmount: true
+        // })
 
+        history.push(`/resume`);
+        // this.scroll.deleteRef();
+
+        
+
+        // let mountNode = ReactDOM.findDOMNode(this.scroll);
+        // let unmount = ReactDOM.unmountComponentAtNode(mountNode);
+
+
+        // this.forceUpdate()
+        // setTimeout(() => {
+        // }, 100);
     }
 
     componentWillMount() {
@@ -50,30 +51,15 @@ class QuestionContainerAxis extends Component {
         if (!company.companyId) {
             history.push(`/`);
         }
-    }
-
-    customProgresiveScroll(event) {
-        console.log(event);
-        event.preventDefault();
-        event.stopPropagation();
-    }
-
-    gotToSection(indexSection) {
-        var elem = this.pollContent.current;
-        var nameNodes = elem.children;
-        console.log("referencia", nameNodes);
-        
 
     }
 
-    pushRef(ref, i) {
-        this.sections[i] = ref;
-    }
 
 
     render() {
 
         const { currentAxis, setCurrentAxis, currentQuests, company } = this.props;
+        const { scroll } = this.state
 
         let nItems = this.state.numItemsPerSection;
 
@@ -86,20 +72,50 @@ class QuestionContainerAxis extends Component {
             .reduce((a, b, i, g) => !(i % nItems) ? a.concat([g.slice(i, i + nItems)]) : a, []);
 
         return (
-            <div ref={this.pollContent} className={`poll-content ${currentAxis}`} onScrollCapture={this.customProgresiveScroll}>
-                <TopSection />
-                {_currentQuests.map((_quest, i) => (
-                    <AspectSection quest={_quest} />
-                ))}
-                <BottomSection goToResume={this.goToResume} gotScrollTop={() => { }} />
-{/* 
+
+
+            <div className={`poll-content ${currentAxis}`} >
+                {company.companyId &&
+                    <Scroller
+                        scrollRef={(ref) => {
+                            this.scroll = ref
+                        }}
+                        autoScroll={true}
+                        autoFrame={true}
+                        onScrollChange={(scroll) => {
+                            let newScroll = Object.assign({}, scroll);
+
+                            let newChildens = newScroll.children.map((scl) => {
+                                if (scl.start > 0) {
+                                    let ofside = ((newScroll.viewHeight - scl.viewHeight) / 2);
+                                    scl.start = scl.start - ofside;
+                                }
+
+                                return scl;
+                            });
+                            newScroll.children = newChildens;
+                            console.log(newScroll);
+                            this.setState({ scroll: newScroll })
+                        }}
+                    >
+                        <TopSection />
+                        {
+                            _currentQuests.map((_quest) => (
+                                <AspectSection quest={_quest} />
+                            ))
+                        }
+                        <BottomSection goToResume={this.goToResume} gotScrollTop={() => this.scroll.scrollToPosition(scroll.start)} />
+                    </Scroller>
+                }
                 <div className="navPanel">
                     {
-                        _currentQuests.map((child, i) =>
-                            <img onClick={() => this.gotToSection(this.sections[i])} src={`resources/${child.active ? 'dot-active' : 'dot'}.svg`} />
+                        scroll.children.map((child, i) =>
+                            <img onClick={() => this.scroll.scrollToPosition(child.start)} src={`resources/${child.active ? 'dot-active' : 'dot'}.svg`} />
                         )
                     }
-                </div> */}
+                </div>
+
+
             </div>
         );
     }
--- src/constantsGlobal.js
+++ src/constantsGlobal.js
@@ -1,18 +1,20 @@
+export const API_SERVER = process.env.HOST_API ? process.env.HOST_API : "http://localhost:3000";
+
 // API
-export const GET_ALL_QUESTIOS_API = "https://biotica-api.herokuapp.com/diagnosis/" ;
-export const GET_VALIDATE_COMPANY_API = "https://biotica-api.herokuapp.com/diagnosis/validateCompany" ;
-export const PUT_ANSWER_API = "https://biotica-api.herokuapp.com/diagnosis/" ;
-export const DELETE_ANSWER_API = "https://biotica-api.herokuapp.com/diagnosis/removeAnswer" ;
+export const GET_ALL_QUESTIOS_API = API_SERVER + "/diagnosis/";
+export const GET_VALIDATE_COMPANY_API = API_SERVER + "/diagnosis/validateCompany";
+export const PUT_ANSWER_API = API_SERVER + "/diagnosis/";
+export const DELETE_ANSWER_API = API_SERVER + "/diagnosis/removeAnswer";
 
 //SAGA -> REDUCERS
-export const GET_ALL_QUESTIOS_SAGA = "GET_ALL_QUESTIOS_SAGA" ;
-export const GET_VALIDATE_COMPANY_SAGA = "GET_VALIDATE_COMPANY_SAGA" ;
-export const PUT_ANSWER_SAGA = "PUT_ANSWER_SAGA" ;
-export const DELETE_ANSWER_SAGA = "DETELE_ANSWER_SAGA" ;
+export const GET_ALL_QUESTIOS_SAGA = "GET_ALL_QUESTIOS_SAGA";
+export const GET_VALIDATE_COMPANY_SAGA = "GET_VALIDATE_COMPANY_SAGA";
+export const PUT_ANSWER_SAGA = "PUT_ANSWER_SAGA";
+export const DELETE_ANSWER_SAGA = "DETELE_ANSWER_SAGA";
 
 // ACTIONS
-export const SET_ALL_QUESTIOS = "SET_ALL_QUESTIOS" ;
-export const SET_CURRENT_AXIS = "SET_CURRENT_AXIS" ;
-export const GET_VALIDATE_COMPANY = "GET_VALIDATE_COMPANY" ;
-export const PUT_ANSWER = "PUT_ANSWER" ;
-export const DELETE_ANSWER = "DELETE_ANSWER" ;
+export const SET_ALL_QUESTIOS = "SET_ALL_QUESTIOS";
+export const SET_CURRENT_AXIS = "SET_CURRENT_AXIS";
+export const GET_VALIDATE_COMPANY = "GET_VALIDATE_COMPANY";
+export const PUT_ANSWER = "PUT_ANSWER";
+export const DELETE_ANSWER = "DELETE_ANSWER";
